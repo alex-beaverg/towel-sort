28e111ce4f9bdacd8b0fb63a817cdd13fb7136fd
alex-beaverg <a.beaverg@gmail.com>
2020-10-06 21:18:34 +0300

    docs: upload task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === [] || matrix === undefined) return [];
+  let result = matrix;
+  let halfRes = []; 
+  for (let i = 1; i < matrix.length; i += 2) {
+        for (let num of matrix[i]) {
+        halfRes.unshift(num);
+    }
+    result[i] = halfRes;
+    halfRes = [];
+  } 
+  
+  return result.flat();
 }
